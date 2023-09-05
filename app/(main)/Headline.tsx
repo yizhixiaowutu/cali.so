1d55032a53dcc5e8717cdbc007d3b5a66d91906c
tianxq3 <tianxq3@ziroom.com>
2023-09-05 16:27:13 +0800

    feat: 需改部分个人信息

--- app/(main)/Headline.tsx
+++ app/(main)/Headline.tsx
@@ -80,7 +80,7 @@ export function Headline() {
         }}
       >
         <Balancer>
-          我是 Cali，
+          我是 一只小於菟，
           <PeekabooLink href="https://zolplay.cn">佐玩</PeekabooLink>
           创始人，目前带领着佐玩致力于创造一个充满创造力的工作环境，同时鼓励团队创造影响世界的产品。
           我热爱开发，设计，创新，享受生活，以及在未知领域中探索。
@@ -109,12 +109,12 @@ export function Headline() {
           platform="youtube"
         />
         <SocialLink
-          href="https://cali.so/bilibili"
+          href="https://space.bilibili.com/385882225"
           aria-label="我的 Bilibili"
           platform="bilibili"
         />
         <SocialLink
-          href="https://cali.so/github"
+          href="https://github.com/yizhixiaowutu"
           aria-label="我的 GitHub"
           platform="github"
         />
--- lib/seo.ts
+++ lib/seo.ts
@@ -1,7 +1,7 @@
 export const seo = {
-  title: 'Cali Castle | 开发者、设计师、细节控、创始人',
+  title: '一只小於菟 | 开发者、音乐爱好者、原批、撸er...',
   description:
-    '我叫 Cali，一名开发者，设计师，细节控，同时也是佐玩创始人，目前带领着佐玩致力于创造一个充满创造力的工作环境，同时鼓励团队创造影响世界的产品。',
+    '我是 一只小於菟 ，一名开发者，细节控，code 只是为了生存，游戏和音乐才是我的热爱~',
   url: new URL(
     process.env.NODE_ENV === 'production'
       ? 'https://cali.so'
